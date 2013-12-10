7b5562dce11dd17163d6d77ca7b8fe7f8e9d9a0c
matteo <matteo.fiandesio@gmail.com>
2013-12-10 16:25:15 +0100

    baasbox theme

--- conf.py
+++ conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,6 +99,8 @@ html_theme = 'default'
 html_theme_options = {
     'nosidebar': True
 }
+html_theme_path = ['_themes']
+html_theme = 'baasbox'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
