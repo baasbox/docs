8637f64fe4eda75f5a86f8402f66734790c68eba
matteo <matteo.fiandesio@gmail.com>
2013-12-10 16:30:38 +0100

    added fake css

--- conf.py
+++ conf.py
@@ -96,9 +96,7 @@ pygments_style = 'sphinx'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'nosidebar': True
-}
+
 html_theme_path = ['_themes']
 html_theme = 'baasbox'
 #html_theme_options = {}
