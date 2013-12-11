4eef8916227717845c270f0dc9b3051e7c582a80
matteo <matteo.fiandesio@gmail.com>
2013-12-11 11:45:52 +0100

    debug js

--- _themes/sphinx_rtd_theme/static/js/theme.js
+++ _themes/sphinx_rtd_theme/static/js/theme.js
@@ -1,18 +1,5 @@
 $( document ).ready(function() {
   // Shift nav in mobile when clicking the menu.
-  $("[data-toggle='bb-nav-top']").click(function() {
-    $("[data-toggle='bb-nav-shift']").toggleClass("shift");
-    $("[data-toggle='rst-versions']").toggleClass("shift");
-  });
-  // Close menu when you click a link.
-  $(".bb-menu-vertical .current ul li a").click(function() {
-    $("[data-toggle='bb-nav-shift']").removeClass("shift");
-    $("[data-toggle='rst-versions']").toggleClass("shift");
-  });
-  var versions = $("[data-toggle='rst-current-version']");
-  console.log(versions,"Versions");
-  versions.on('click',function() {
-    $("[data-toggle='rst-versions']").toggleClass("shift-up");
-  });
-  $("table.docutils:not(.field-list").wrap("<div class='bb-table-responsive'></div>");
+  console.log("Empty")
+  
 });
